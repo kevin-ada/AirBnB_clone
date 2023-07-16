e6e65382218ca5e745f49bb369cfa7dce3b2030d
kim-thuita <kevinkoech265@gmail.com>
2023-07-17 01:43:48 +0300

    "Fixing file updates and errors"

--- console.py
+++ console.py
@@ -1,208 +1,266 @@
 #!/usr/bin/python3
-"""Implementation of a cmd Module"""
+"""Console intepreter for AirBNB clone"""
 import cmd
-import json
-import shlex
-import re
-from models.engine.file_storage import FileStorage
-
-classes = {'BaseModel', 'User', 'Place', 'State', 'City', 'Amenity', 'Review'}
-
-
-def get_content(args):
-    """Returns the '(<content>)' from a given string
-    """
-    return args[args.rfind("(") + 1:args.rfind(")")]
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+import models
 
 
 class HBNBCommand(cmd.Cmd):
-    """Class that implements the HolbertonBnb cmd
-
-    Args:
-        prompt ([str]): [Prompt to show]
-
-    Returns:
-        [type]: [infinite loop]
     """
-    prompt = "(hbnb) "
+    Create class HBNBCommand
+    """
+    prompt = "(hbnb)"  # Set your custom prompt here
+    classes = [
+            'BaseModel', 'Amenity', 'City', 'Place', 'Review', 'State', 'User']
+
+    def do_EOF(self, line):
+        """Checks end of file"""
+        return True
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Do nothing if empty line is passed"""
+        pass
 
     def do_create(self, args):
-        """creates an instance of a given class, saves it to a JSON file"""
-        args = shlex.split(args)
+        """Creates a new instance"""
+        if args:
+            if args in self.classes:
+                # class_obj = getattr(base_model, args)
+                dummy_instance = eval(args)()
+                dummy_instance.save()
+                print(dummy_instance.id)
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+    def do_show(self, line):
+        """Prints string representation of an instance"""
+        args = line.split()
+
         if not args:
             print("** class name missing **")
             return
-        if args[0] not in classes:
+
+        class_name = args[0]
+
+        if class_name not in self.classes:
             print("** class doesn't exist **")
             return
-        obj = eval(args[0])()
-        obj.save()
-        print(obj.id)
 
-    def do_show(self, args):
-        """method retrieves and prints the string
-        representation of an instance based on the class name and ID."""
-        inputs = shlex.split(args)
-        if not inputs:
-            print('** class name missing **')
-        elif inputs[0] not in classes:
-            print("** class doesn't exist **")
-        elif len(inputs) < 2:
-            print("** instance id missing **")
-        elif '{}.{}'.format(inputs[0], inputs[1]) not in FileStorage.all(self):
-            print("** no instance found **")
-        else:
-            key = '{}.{}'.format(inputs[0], inputs[1])
-            content = FileStorage.all(self)[key]
-            print(content)
-
-    def do_destroy(self, args):
-        """method deletes an instance
-        based on its ID."""
-        inputs = shlex.split(args)
-        if not inputs:
-            print('** class name missing **')
-        elif inputs[0] not in classes:
-            print("** class doesn't exist **")
-        elif len(inputs) < 2:
-            print("** instance id missing **")
-        elif '{}.{}'.format(inputs[0], inputs[1]) not in FileStorage.all(self):
-            print("** no instance found **")
-        else:
-            key = '{}.{}'.format(inputs[0], inputs[1])
-            del FileStorage.all(self)[key]
-            FileStorage.save(self)
-
-    def do_all(self, args):
-        """ prints the string
-        representation of all instances of a given
-         class or all instances in general."""
-        args = shlex.split(args)
-        dict_1 = FileStorage.all(self)
-
-        if not args:
-            print([str(value) for value in dict_1.values()])
-            return
-        if args[0] in classes:
-            print([str(value) for key, value in dict_1.items()
-                   if key.split(".")[0] == args[0]])
-            return
-        print("** class doesn't exist **")
-
-    def do_update(self, args):
-        """ method updates an instance based on the class name,
-         ID, attribute name, and new value.
-        """
-
-        integers = {'number_rooms', 'number_bathrooms',
-                    'max_guest', 'price_by_night'}
-        floats = {'latitude', 'longitude'}
-        args = shlex.split(args)
-        if not args:
-            print("** class name missing **")
-            return
-        if args[0] not in classes:
-            print("** class doesn't exist **")
-            return
         if len(args) < 2:
             print("** instance id missing **")
             return
-        if '{}.{}'.format(args[0], args[1]) not in FileStorage.all(self):
+
+        instance_id = args[1]
+        key = class_name + "." + instance_id
+
+        if key in models.storage.all():
+            print(models.storage.all()[key])
+        else:
+            print("** no instance found **")
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+        args = line.split()
+
+        if not args:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+
+        if class_name not in self.classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args[1]
+        key = class_name + "." + instance_id
+
+        if key in models.storage.all():
+            del models.storage.all()[key]
+            models.storage.save()
+        else:
+            print("** no instance found **")
+    """
+    def do_all(self, line):
+        Prints all string representation of all
+        instances based or not on the class name
+        list_objs = []
+        if line in self.classes:
+            objs = models.storage.all()
+            for inst_name_id, inst in objs.items():
+                cls_name_id = inst_name_id.split(".")
+                if line == cls_name_id[0]:
+                    list_objs.append(str(inst))
+            print(list_objs)
+        else:
+            print("** class doesn't exist **")
+    """
+    def do_all(self, line):
+        """
+        Prints all string representation of all
+        instances based or not on the class name
+        """
+        if line in self.classes:
+            objects = models.storage.all()
+            class_instances = [
+                str(instance) for instance in objects.values()
+                if type(instance).__name__ == line]
+            print(class_instances)
+        elif not line:
+            objects = models.storage.all()
+            all_instances = [str(instance) for instance in objects.values()]
+            print(all_instances)
+        else:
+            print("** class doesn't exist **")
+
+    def do_count(self, line):
+        """Counts the number of instances of a class"""
+        if line in self.classes:
+            objects = models.storage.all()
+            class_instances = [
+                instance for instance in objects.values()
+                if type(instance).__name__ == line]
+            count = len(class_instances)
+            print(count)
+        else:
+            print("** class doesn't exist **")
+
+    def custom_split(self, line):
+        """
+        Helper function for update
+        splits classname and id
+        """
+        args = []
+        current_arg = ""
+        inside_quotes = False
+
+        for char in line:
+            if char == '"':
+                inside_quotes = not inside_quotes
+            elif char == ' ' and not inside_quotes:
+                if current_arg:
+                    args.append(current_arg)
+                    current_arg = ""
+                continue
+
+            current_arg += char
+
+        if current_arg:
+            args.append(current_arg)
+
+        return args
+
+    def do_update(self, line):
+        """
+        Updates an instance based on the class name
+        and id by adding or updating attribute
+        """
+        args = self.custom_split(line)
+
+        if not args:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+
+        if class_name not in self.classes:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = args[1]
+        key = class_name + "." + instance_id
+
+        if key not in models.storage.all():
             print("** no instance found **")
             return
+
         if len(args) < 3:
             print("** attribute name missing **")
             return
+
         if len(args) < 4:
             print("** value missing **")
             return
-        if args[2] in floats:
-            try:
-                args[3] = float(args[3])
-            except ValueError:
-                args[3] = 0.0
-        if args[2] in integers:
-            try:
-                args[3] = int(args[3])
-            except ValueError:
-                args[3] = 0
-        content = FileStorage.all(self)['{}.{}'.format(args[0], args[1])]
-        setattr(content, args[2], args[3])
-        content.save()
 
-    def emptyline(self) -> bool:
-        """ method is called when the user
-         enters an empty line,
-          and it does nothing."""
-        pass
+        attribute_name = args[2]
+        attribute_value = args[3]
+        instance = models.storage.all()[key]
 
-    def do_quit(self, arg):
-        """methods handle the commands to exit the program."""
-        return True
+        try:
+            attribute_value = eval(attribute_value)
+        except (NameError, SyntaxError):
+            pass
 
-    def do_EOF(self, arg):
-        """methods handle the commands to exit the program."""
-        print("")
-        return True
+        setattr(instance, attribute_name, attribute_value)
+        models.storage.save()
 
     def default(self, line):
-        """
-         the entered command doesn't match any existing method.
-         It parses the command
-         using regular expressions and
-         invokes the corresponding method dynamically.
-        """
-        args = line.split('.')
-        if len(args) < 2 or all([True if element == ""
-                                 else False for element in args]):
-            print(f"*** Unknown syntax: {line}")
-            return False
-
-        string = "self.do_"
-        if args[1] == 'all()':
-            string += f"all('{args[0]}')"
-            eval(string)
-        elif 'show(' in args[1]:
-            arguments = get_content(args[1]).split(',')
-            string += "show('{} {}')".format(args[0],
-                                             arguments[0].replace("'", '"'))
-            eval(string)
-        elif 'count(' in args[1]:
-            string += "count('{}')".format(args[0])
-            eval(string)
-        elif 'destroy(' in args[1]:
-            arguments = get_content(args[1]).split(',')
-            string += "destroy('{} {}')".format(args[0],
-                                                arguments[0].replace("'", '"'))
-            eval(string)
-        elif 'update(' in args[1]:
-            if "{" in get_content(args[1]) and "}" in get_content(args[1]):
-                coincidence = re.search(
-                    r"\{.*?}", line).group().replace("'", '"')
-                dictionary = json.loads(coincidence)
-                id = re.search(r'\".*?\"', line).group()
-                [self.do_update('{} {}  {}  {}'.
-                                format(args[0], id, key, value))
-                 for key, value in dictionary.items()]
-
-            else:
-                arguments = get_content(args[1]).split(',')
-                string += """update('{} {} {} {}')""".format(
-                    args[0], arguments[0].replace("'", '"'),
-                    arguments[1].replace("'", '"'),
-                    arguments[2].replace("'", '"'))
-                eval(string)
-
-    def do_count(self, args):
-        """
-        method retrieves the number of instances of a specific class.
-        """
-        args = shlex.split(args)
-        counter = 0
-        for obj in FileStorage.all(self).values():
-            if args[0] == obj.__class__.__name__:
-                counter += 1
-        print(counter)
+        """Default command handler method"""
+        if line.endswith(".all()"):
+            class_name = line[:-6]
+            self.do_all(class_name)
+        elif line.endswith(".count()"):
+            class_name = line[:-8]
+            self.do_count(class_name)
+        elif ".show" in line:
+            for class_name in self.classes:
+                if line.startswith(
+                        f"{class_name}.show(") and line.endswith(")"):
+                    start_index = line.find("(")
+                    end_index = line.find(")")
+                    instance_id = line[start_index + 2: end_index - 1]
+                    line = "{} {}".format(class_name, instance_id)
+                    self.do_show(line)
+                    return
+        elif ".destroy" in line:
+            for class_name in self.classes:
+                if line.startswith(
+                        f"{class_name}.destroy(") and line.endswith(")"):
+                    start_index = line.find("(")
+                    end_index = line.find(")")
+                    instance_id = line[start_index + 2: end_index - 1]
+                    line = "{} {}".format(class_name, instance_id)
+                    self.do_destroy(line)
+                    return
+        elif ".update" in line:
+            for class_name in self.classes:
+                if line.startswith(
+                        f"{class_name}.update(") and line.endswith(")"):
+                    start_index = line.find("(")
+                    end_index = line.find(")")
+                    params = line[start_index + 1: end_index].split(", ")
+                    if len(params) >= 3:
+                        inst_id = params[0][1:-1]
+                        attr_name = params[1][1:-1]
+                        attr_val = params[2][1:-1]
+                        line = f"{class_name} {inst_id} {attr_name} {attr_val}"
+                        self.do_update(line)
+                        return
+                    else:
+                        print("*** Missing parameters for update ***")
+                        return
+        else:
+            print("*** Unknown syntax: {}".format(line))
 
 
 if __name__ == '__main__':
